b4ff4266b457bdcf82d01766a2d9b3ef55699392
hydai <hydai@skymizer.com>
2018-07-18 11:34:21 +0800

    rpc: increase timeout of http server

--- rpc/http.go
+++ rpc/http.go
@@ -167,8 +167,8 @@ func NewHTTPServer(cors []string, vhosts []string, srv *Server) *http.Server {
 	handler = newVHostHandler(vhosts, handler)
 	return &http.Server{
 		Handler:      handler,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
 		IdleTimeout:  120 * time.Second,
 	}
 }
